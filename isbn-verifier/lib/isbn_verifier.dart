b31847e0d6db89e166a2eec1c4cb8c4bef3c8eca
vachtung-gigabidze <dmitry.zyadik@gmail.com>
2022-08-01 12:03:23 +0500

    complete isbn_verifier

--- isbn-verifier/lib/isbn_verifier.dart
+++ isbn-verifier/lib/isbn_verifier.dart
@@ -1,4 +1,27 @@
-// Put your code here
 bool isValid(String s) {
-  return true;
+  return isMatch(s) ? verifier(parse(s)) : false;
+}
+
+bool isMatch(String s) {
+  RegExp re = RegExp(r'^(\d{1}-?\d{3}-?\d{5}-?[\d|X]{1})$');
+  return re.hasMatch(s);
+}
+
+List<int> parse(String s) {
+  s = s.replaceAll('-', '');
+  return s.split('').map((e) {
+    e = e.replaceAll('X', '10');
+    return int.parse(e);
+  }).toList();
+}
+
+bool verifier(List<int> ar) {
+  return ar
+              .asMap()
+              .map<int, int>(
+                  (key, value) => MapEntry(key, value * (ar.length - (key))))
+              .values
+              .reduce((value, element) => value + element) %
+          11 ==
+      0;
 }
